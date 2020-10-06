9aa8baca57440265fbfc889427fb7b94c17750dd
bevonbrye <bryebevon@gmail.com>
2020-10-06 11:08:05 -0700

    add constant variables

---
+++ JS/ardor.js
@@ -0,0 +1,12 @@
+//constants: 
+//create array of words to be used
+const arrayOfWords = ['ardor', 'sabaism', 'latibuli', 'chrysalism', 'astrophile', 'psithurism', 'kalokagathia', 'ataraxia', 'eurneirophrenia', 'kalopsia', 'mogoa', 'whelve', 'dormiveglia', 'aeipathy', 'woodnote', 'pulchritudinous', 'nemophilst'];
+const easyWrongGuesses = 8;
+const medWrongGuesses = 6;
+const hardWrongGuesses = 3;
+
+
+
+
+
+// functions:
