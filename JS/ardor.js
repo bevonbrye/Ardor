007cbf59787a07c12caa3340c6b203fc452c8643
bevonbrye <bryebevon@gmail.com>
2020-10-06 22:11:16 -0700

    add startGame logic

--- JS/ardor.js
+++ JS/ardor.js
@@ -1,14 +1,16 @@
-//extra: 
-let container = document.querySelector('container')
+//extra: ignore for now 
+// let container = document.querySelector('container')
 
-let createPic = document.createElement('img');
-createPic.setAttribute('src', './human.png');
-createPic.setAttribute('class', 'createPic');
-container.appendChild(createPic)
+// let createPic = document.createElement('img');
+// createPic.setAttribute('src', './human.png');
+// createPic.setAttribute('class', 'createPic');
+// container.appendChild(createPic)
 
-//const and Lets : 
+
+//const and Lets: 
 //create array of words to be used
 const arrayOfWords = ['ardor', 'sabaism', 'latibuli', 'chrysalism', 'astrophile', 'psithurism', 'kalokagathia', 'ataraxia', 'eurneirophrenia', 'kalopsia', 'mogoa', 'whelve', 'dormiveglia', 'aeipathy', 'woodnote', 'pulchritudinous', 'nemophilst'];
+//consts and lets: 
 const easyWrongGuesses = 8;
 const medWrongGuesses = 6;
 const hardWrongGuesses = 3;
@@ -21,6 +23,14 @@ let userGuess;
 // functions:
 //start game funtion that will enable all the varibles that will change:
 function startGame() {
-    console.log('this works')
+    lettersGuessed = [];
+    //I need to get a random number from the array of words 
+    const getRandomIndex = Math.floor(Math.random() * arrayOfWords.length)
+    arrWord = arrayOfWords[getRandomIndex];
+    // I need to loop through the array of words 
+    userGuess = '';
+    for (let letter of arrWord) {
+        userGuess += letter === ' ' ? ' ' : '_'; // ter op easier way to use condits
+    }
 };
 startGame();
