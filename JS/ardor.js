cc2e8dea11fc465f25dd4c4014458d02a84f901f
bevonbrye <bryebevon@gmail.com>
2020-10-09 02:43:10 -0700

    refactor, remove comments, display letters

--- JS/ardor.js
+++ JS/ardor.js
@@ -7,22 +7,13 @@
 // container.appendChild(createPic)
 document.querySelector('section').addEventListener('click', clickLetter);
 const usersGuess = document.querySelector('.usersGuess')
-    //const and Lets: 
-    //create array of words to be used
-const arrayOfWords = ['ardor', 'sabaism', 'latibuli', 'chrysalism', 'astrophile', 'psithurism', 'kalokagathia', 'ataraxia', 'eurneirophrenia', 'kalopsia', 'mogoa', 'whelve', 'dormiveglia', 'aeipathy', 'woodnote', 'pulchritudinous', 'nemophilst'];
-//consts and lets: 
+const arrayOfWords = ['cat', 'dog'] //[, 'sabaism', 'latibuli', 'chrysalism', 'astrophile', 'psithurism', 'kalokagathia', 'ataraxia', 'eurneirophrenia', 'kalopsia', 'mogoa', 'whelve', 'dormiveglia', 'aeipathy', 'woodnote', 'pulchritudinous', 'nemophilst'];
 const easyWrongGuesses = 8;
 const medWrongGuesses = 6;
 const hardWrongGuesses = 3;
-
-//create varible that change during the game:
 let arrWord;
 let lettersGuessed;
 let userGuess;
-
-// functions:
-//start game funtion that will enable all the varibles that will change:
-
 
 startGame();
 
@@ -30,13 +21,11 @@ function clickLetter(e) {
     const letter = e.target.innerText;
     if (letter.length > 1 || gameOver() || lettersGuessed.includes(letter) || userGuess.includes(letter))
         return;
-    //------------------------------
     if (arrWord.includes(letter)) {
         let guess = '';
         for (let i = 0; i < arrWord.length; i++) {
             guess += arrWord.charAt(i) === letter ? letter : userGuess.charAt(i)
         }
-        //-------------------------
         userGuess = guess;
     } else {
         lettersGuessed.push(letter);
@@ -45,16 +34,13 @@ function clickLetter(e) {
 }
 
 function gameOver() {
-    return arrWord === userGuess || lettersGuessed.length === easyWrongGuesses;
-
+    return arrWord === userGuess || lettersGuessed.length === hardWrongGuesses
 }
 
 function startGame() {
     lettersGuessed = [];
-    //I need to get a random number from the array of words 
     const getRandomIndex = Math.floor(Math.random() * arrayOfWords.length)
     arrWord = arrayOfWords[getRandomIndex];
-    // I need to loop through the array of words 
     userGuess = '';
     displayItems();
 };
